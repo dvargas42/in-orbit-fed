d7bce97ae0c56896e19037ec95f1f6eca4de6c36
dvargas42 <dvargas.eng@gmail.com>
2024-09-15 01:41:57 -0300

    feat: :tada: initial commit

---
+++ src/app.tsx
@@ -0,0 +1,21 @@
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { WeeklySummary } from './components/weekly-summary'
+import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+
+export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+  })
+
+  return (
+    <Dialog>
+      {data && data.total > 0 ? <WeeklySummary /> : <EmptyGoals />}
+      <CreateGoal />
+    </Dialog>
+  )
+}
